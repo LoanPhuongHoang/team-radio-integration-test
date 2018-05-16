d1e311e08acf21612d86f6e447c541704ae9ef0b
LoanPhuongHoang <hoangphuongloan1304@gmail.com>
2018-05-16 10:20:01 +0700

    SortStation

---
+++ src/gebtest/SortStationGebSpec.groovy
@@ -0,0 +1,14 @@
+//package gebtest
+//
+//import gebtest.multiuser.MultiUserGebSpec
+//
+//class SortStationGebSpec extends MultiUserGebSpec {
+//	def 'sort multi stations'(){
+//		given:
+//		def (stationOwner, user1, user2, anonUser1, anonUser2) = users
+//
+//		and:
+//
+//
+//	}
+//}
